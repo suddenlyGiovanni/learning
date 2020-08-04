19ae6850d0b8202887ca1d3d13eaba97816f1844
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-04 19:35:57 +0200

    feat(algo): add linear search on unsorted list

---
+++ packages/a-practical-introduction-to-algorithms/src/15-linear-search.ts
@@ -0,0 +1,43 @@
+// TASK: Implement linear search.
+
+const NOT_FOUND = -1
+/**
+ * A linear search fn that works for collection of string and number
+ * ISSUE: this elegant implementation cleverly uses reduce/fold to accomplish the search.
+ * reduce iterate through out the all collection. it does not stop when it encounters the matching
+ * condition. (for a collection of length n, it has to iterate till n => time complexity O(n))
+ * @param {A[]} xs
+ * @param {A} item
+ * @returns {number | -1} the position of the last item match in the collection
+ */
+export const linearSearchFold = <A extends string | number>(
+  xs: A[],
+  item: A
+): number | -1 =>
+  xs.reduce((acc, x, i) => {
+    return x === item ? i : acc
+  }, NOT_FOUND)
+
+/**
+ * @template A
+ * @param {A[]} xs
+ * @param {A} item
+ * @returns {number | -1} the position of the first item match in the collection
+ */
+export const linearSearch = <A extends string | number>(
+  xs: A[],
+  item: A
+): number | -1 => {
+  let index = NOT_FOUND
+
+  for (let i = 0; i < xs.length; i++) {
+    if (xs[i] === item) {
+      index = i
+      break
+    }
+  }
+  return index
+}
+
+linearSearchFold([2, 6, 7, 90, 103, 90], 90) //?
+linearSearch([2, 6, 7, 90, 103, 90], 90) //?
