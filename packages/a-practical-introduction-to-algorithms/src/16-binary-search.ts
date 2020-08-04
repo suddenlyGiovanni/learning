e288355ba3bffc03c4d06af29e8aba46afeae90f
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-04 20:15:49 +0200

    refactor(algo): add recursive binary search

--- packages/a-practical-introduction-to-algorithms/src/16-binary-search.ts
+++ packages/a-practical-introduction-to-algorithms/src/16-binary-search.ts
@@ -1,29 +1,53 @@
-/* eslint-disable init-declarations, max-statements */
+/* eslint-disable init-declarations, max-statements, spaced-comment */
 // TASK: Implement binary search.
 
 export const binarySearch = <A>(list: A[], item: A): -1 | number => {
   // Set up the bounds
   let min = 0
   let max = list.length - 1
-  let guess: number
+  let index: number
 
   while (min <= max) {
     // Pick an element in the middle
-    guess = Math.floor((max + min) / 2)
-    if (list[guess] === item) {
+    index = Math.floor((max + min) / 2)
+    if (list[index] === item) {
       // Jackpot
-      return guess
+      return index
     }
-    if (item < list[guess]) {
+    if (item < list[index]) {
       // Left side
-      max = guess - 1
+      max = index - 1
     } else {
       // Right side
-      min = guess + 1
+      min = index + 1
     }
   }
 
   return -1
 }
 
-binarySearch([2, 6, 7, 90, 103], 90) //?
+export const recursiveBinarySearch = <A>(list: A[], item: A): -1 | number => {
+  const recurse = (min: number, max: number): -1 | number => {
+    // Pick an index at the middle
+    const index = Math.floor((min + max) / 2) //?
+    // Verify if the item is smaller or bigger than the element at the center
+    if (min > max) {
+      //  Bottom case the item is not present in the list
+      return -1
+    }
+    if (item === list[index]) {
+      // Jackpot! we have found the item
+      return index
+    } else if (item < list[index]) {
+      // Item has to be located in the left part of the sub-list
+      return recurse(min, index - 1)
+    }
+    // Item has to be located in the right part of the sub-list
+    return recurse(index + 1, max)
+  }
+
+  return recurse(0, list.length - 1) //?
+}
+
+binarySearch([2, 6, 7, 90, 103, 120], 90) //?
+recursiveBinarySearch([2, 6, 7, 90, 103, 120], 90) //?
