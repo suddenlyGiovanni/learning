c5f54c7df529586f9a0236d30e93dff529de401e
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-05 21:01:13 +0200

    feat(algo): add recursive version of bubble sort

--- packages/a-practical-introduction-to-algorithms/src/17-buble-sort.ts
+++ packages/a-practical-introduction-to-algorithms/src/17-buble-sort.ts
@@ -1,4 +1,19 @@
 /* eslint-disable no-underscore-dangle, max-statements, spaced-comment */
+/**
+ * # Bubble sort
+ *
+ * Bubble sort is an algorithm that compares the adjacent elements and swaps their positions if
+ * they are not in the intended order. The order can be ascending or descending.
+ * 1 - Starting from the first index, compare the first and the second elements.
+ *     If the first element is greater than the second element, they are swapped.
+ *     Now, compare the second and the third elements. Swap them if they are not in order.
+ *     The above process goes on until the last element.
+ * 2 - The same process goes on for the remaining iterations.
+ *     After each iteration, the largest element among the unsorted elements is placed at the end.
+ *     In each iteration, the comparison takes place up to the last unsorted element.
+ *     The array is sorted when all the unsorted elements are placed at their correct positions.
+ */
+
 /*
  * # Bubble sort pseudo code
  *
@@ -54,5 +69,35 @@ export const bubbleSort = <T>(list: T[]): T[] => {
   return _list
 }
 
+export const recursiveBubbleSort = <A>(xs: readonly A[]): A[] => {
+  const _xs = xs.slice()
+  const swap = <T>(leftIdx: number, rightIdx: number, array: T[]): T[] => {
+    const left = array[leftIdx]
+    array[leftIdx] = array[rightIdx]
+    array[rightIdx] = left
+    return array
+  }
+
+  type sort = <A>(ys: readonly A[], n: number) => A[]
+  const sort: sort = (ys, n) => {
+    const _ys = ys.slice()
+    _ys.forEach((left, leftIdx, __ys) => {
+      const rightIdx = leftIdx + 1
+      const right = __ys[rightIdx]
+      // If left is grater then right then swap them
+
+      if (left > right) {
+        swap(leftIdx, rightIdx, __ys)
+      }
+      // Else do nothing
+    })
+
+    return n - 1 > 1 ? sort(_ys, n - 1) : _ys
+  }
+
+  return sort(_xs, _xs.length - 1)
+}
+
 // [1, 2, 4, 5, 8]
 bubbleSort([5, 1, 4, 2, 8]) //?
+recursiveBubbleSort([5, 1, 4, 2, 8]) //?
