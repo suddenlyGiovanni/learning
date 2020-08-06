3f24e8a48d296da6c55f37ba266e3f2639515a47
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-07 01:48:38 +0200

    feat(algo): add exercise on a gready algo

---
+++ packages/a-practical-introduction-to-algorithms/src/21-greedy-exercise-1.ts
@@ -0,0 +1,44 @@
+/* eslint-disable max-statements, id-length */
+/**
+ * Write a function, makeChange, that returns an integer that represents the least number of coins
+ * that add up to an amount where the amount is always divisible by 5.
+ *
+ * Coin values: 5, 10, 25
+ *
+ * Input amount: 40 , output # of coins: 3 (25, 10, 5)
+ *
+ * Input amount: 35, output # of coins: 2 (25, 10)
+ */
+
+type int = number
+export const makeChange = (coins: readonly int[], amount: int): int[] => {
+  const coinSet = coins.slice().sort((x, y) => y - x)
+  const canSubtract = (a: int, b: int): boolean => a - b >= 0
+  const subtract = (a: int, b: int): int => a - b
+  const accumulator: int[] = []
+  let remainder = amount
+
+  while (remainder > 0) {
+    const [head, ...tail] = coinSet
+
+    // There are still coins to be used
+    if (head) {
+      // Can subtract the bigger cut available from the reminder
+      if (canSubtract(remainder, head)) {
+        // Subtract
+        remainder = subtract(remainder, head)
+        // Add the coin to the list of used coins
+        accumulator.push(head)
+      } else {
+        // If the bigger cut could not be use then remove it from the available coin set
+        coinSet.shift()
+      }
+    }
+  }
+
+  return accumulator
+}
+
+makeChange([5, 10, 25], 50) // ?
+makeChange([5, 10, 25], 40) // ?
+makeChange([5, 10, 25], 35) // ?
