7540448a1d3712f633ceaf6460cc64bf05829199
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-09 18:34:12 +0200

    refactor(data-structure): switch to Record<number, T> as underlying data struct for Queue

--- packages/practical-intro-to-data-structures/src/queue.ts
+++ packages/practical-intro-to-data-structures/src/queue.ts
@@ -1,4 +1,6 @@
-/* eslint-disable no-underscore-dangle, no-undefined, no-inline-comments, @typescript-eslint/ban-ts-comment */
+/* eslint-disable spaced-comment */
+/* eslint-disable max-statements */
+/* eslint-disable no-underscore-dangle, no-undefined, no-inline-comments, @typescript-eslint/ban-ts-comment, no-plusplus */
 
 /**
  * A Queue is a collection of entities that are maintained in a sequence and can be modified by the
@@ -39,42 +41,173 @@ export interface QueueInterface<T> {
 }
 
 export class Queue<T> implements QueueInterface<T> {
-  private readonly _queue: T[]
+  private readonly _queue: Record<number, T>
+
+  // 1,2,3,4
+  private _length: number
+
+  // Zero base index
+  private _headIndex: null | number
 
   public constructor() {
-    this._queue = []
+    this._queue = {}
+    this._headIndex = null
+    this._length = 0
   }
 
-  public enqueue(x: T): void {
-    this._queue.push(x)
+  private incrementLength(): void {
+    this._length++
+  }
+
+  private decrementLength(): void {
+    this._length--
+  }
+
+  private deleteElementFromQueue(index: number): void {
+    delete this._queue[index]
+  }
+
+  public enqueue(value: T): void {
+    if (this._headIndex === null) {
+      /*
+       * Head has not been set yet.
+       * manually set the head to the start!
+       */
+      this._headIndex = 0
+      const tail = this._headIndex
+      this._queue[tail] = value
+      this.incrementLength()
+    } else {
+      // We just have to add to the tail
+      const tail = this._length
+      this._queue[tail] = value
+      this.incrementLength()
+    }
   }
 
   public dequeue(): T | undefined {
-    if (this._queue.length > 0) {
-      return this._queue.shift() as T
+    if (this._headIndex === null || this._length === 0) {
+      return undefined
     }
-    return undefined
+
+    if (this._length === 1) {
+      const head = this._queue[this._headIndex]
+      this.deleteElementFromQueue(this._headIndex)
+      this.decrementLength()
+      this._headIndex = null
+      return head
+    }
+
+    const head = this._queue[this._headIndex]
+    this.deleteElementFromQueue(this._headIndex)
+    this.decrementLength()
+    this._headIndex++
+    return head
   }
 
   public peek(): T | undefined {
-    if (this._queue.length > 0) {
-      return this._queue[0]
+    if (this._headIndex === null) {
+      return undefined
     }
-    return undefined
+    return this._queue[this._headIndex]
+  }
+
+  public toString(): string {
+    return JSON.stringify(this)
   }
 }
 
 // Example
 
 const testQueue = new Queue<string>()
-testQueue.enqueue('first')
-testQueue.enqueue('second')
-testQueue.enqueue('third')
-testQueue.enqueue('forth')
-testQueue.peek() // => 'first'
-testQueue.dequeue() // => 'first'
-testQueue.dequeue() // => 'second'
-testQueue.dequeue() // => 'third'
-testQueue.dequeue() // => 'forth'
-testQueue.peek() // => Error('Queue underflow')
-testQueue.dequeue() // => Error('Queue underflow')
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": null,
+ *   "queue": {},
+ *   "length": 0
+ * }
+ */
+
+testQueue.enqueue('zero')
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 0
+ *   "queue": {"0": "zero"},
+ *   "length": 1
+ * }
+ */
+
+testQueue.enqueue('one')
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 0
+ *   "queue": {"0": "zero", "1": "one"},
+ *   "length": 2
+ * }
+ */
+
+testQueue.enqueue('two')
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 0
+ *   "queue": {"0": "zero", "1": "one", "2": "two"},
+ *   "length": 3
+ * }
+ */
+
+testQueue.peek() // 'zero'
+testQueue.dequeue() // 'zero'
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 1
+ *   "queue": {"1": "one", "2": "two"},
+ *   "length": 2
+ * }
+ */
+
+testQueue.peek() // 'one'
+testQueue.dequeue() // 'one'
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 2
+ *   "queue": {"2": "two"},
+ *   "length": 1
+ * }
+ */
+
+testQueue.peek() // 'two'
+testQueue.dequeue() // 'two'
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": null
+ *   "queue": {},
+ *   "length": 0
+ * }
+ */
+
+testQueue.enqueue('three')
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 0
+ *   "queue": {"0": "three"},
+ *   "length": 1
+ * }
+ */
+
+testQueue.enqueue('four')
+testQueue.toString() // ?
+/*
+ * {
+ *   "head": 0
+ *   "queue": {"0": "three", "1": "four"},
+ *   "length": 2
+ * }
+ */
