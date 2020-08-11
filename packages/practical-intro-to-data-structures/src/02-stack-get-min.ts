ad5ce4296c7cea2b8a94c950dbef49f219e96b26
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-11 21:41:22 +0200

    feat(data-structure): add min max Stack<T> exercise

---
+++ packages/practical-intro-to-data-structures/src/02-stack-get-min.ts
@@ -0,0 +1,182 @@
+/* eslint-disable max-statements, no-unused-expressions, no-underscore-dangle */
+import { Stack as IStack, StackClass } from './stack'
+
+/**
+ * # Exercise:
+ * Implement the a min method for the Stack class below.
+ */
+
+interface Min<T> {
+  /*
+   * Returns the min in the stack without removing it
+   * @return {undefined | T} the minimum value in the stack
+   */
+  min(): undefined | T
+}
+
+interface Max<T> {
+  /*
+   * Returns the `max` in the stack without removing it
+   * @return {undefined | T} the maximum value in the stack
+   */
+  max(): undefined | T
+}
+
+/**
+ * Stack class
+ * Time: push O(1), pop O(1), peek O(1), min O(1)
+ * @constructor
+ * @template T
+ */
+export default class Stack<T> implements IStack<T>, Min<T>, Max<T> {
+  private readonly _maxStack: IStack<{ value: T }>
+
+  private readonly _minStack: IStack<{ value: T }>
+
+  private readonly _storage: { value: T }[]
+
+  public constructor() {
+    this._storage = []
+    this._minStack = new StackClass<{ value: T }>()
+    this._maxStack = new StackClass<{ value: T }>()
+  }
+
+  /*
+   * Returns the `max` in the stack without removing it
+   * @return {undefined | T} the maximum value in the stack
+   */
+  public max(): T | undefined {
+    return this._maxStack.peek()?.value
+  }
+
+  /*
+   * Returns the min in the stack without removing it
+   * @return {undefined | T} the minimum value in the stack
+   */
+  public min(): undefined | T {
+    return this._minStack.peek()?.value
+  }
+
+  /*
+   * Returns the value at the end of the stack without removing it
+   * @return {undefined | T} the last and newest value in the stack
+   */
+  public peek(): undefined | T {
+    if (!this.isEmpty()) {
+      const item = this._storage[this._storage.length - 1]
+      return item.value
+    }
+    return undefined
+  }
+
+  /*
+   * Removes the value at the end of the stack and returns it
+   * @return {undefined | T} the last and newest value in the stack
+   */
+  public pop(): undefined | T {
+    if (!this.isEmpty()) {
+      const minFrame = this._minStack.peek()
+      const maxFrame = this._maxStack.peek()
+      const frame = this._storage.pop()
+      if (minFrame === frame) {
+        this._minStack.pop()
+      }
+      if (maxFrame === frame) {
+        this._maxStack.pop()
+      }
+      return frame?.value
+    }
+    return undefined
+  }
+
+  /*
+   * Adds a new value at the end of the stack
+   * @param {T} value - the value to push
+   */
+  public push(value: T): void {
+    const frame = { value }
+    const min = this._minStack.peek()
+    const max = this._maxStack.peek()
+
+    if (this._storage.length === 0) {
+      // We are setting the head!
+      this._minStack.push(frame)
+      this._maxStack.push(frame)
+    }
+
+    // We need to check if the current frame is smaller than the one on the min stack
+    if (min && value < min.value) {
+      this._minStack.push(frame)
+    }
+
+    if (max && value > max.value) {
+      this._maxStack.push(frame)
+    }
+
+    /*
+     * FIXME: max and min checks are performed on non inclusive values;
+     * this can lead to set a frame in a previous operation on one of the accessory stacks and then
+     * not be able to clear on this operation coz of a different reference of the obj.
+     */
+
+    this._storage.push(frame)
+  }
+
+  /*
+   * Returns if the stack is empty or not
+   * @return {boolean} - whether or not the stack is empty
+   */
+  private isEmpty(): boolean {
+    return this._storage.length === 0
+  }
+}
+
+/*
+ * Examples:
+ * const testMinStack = new Stack<number>()
+ * --------------
+ * testMinStack.push(10)
+ * --------------
+ * testMinStack.min() //?
+ * testMinStack.max() //?
+ * --------------
+ * testMinStack.push(9)
+ * testMinStack.min() //?
+ * testMinStack.max() //?
+ * --------------
+ * testMinStack.push(11)
+ * testMinStack.min() //?
+ * testMinStack.max() //?
+ * --------------
+ * testMinStack['_storage'] //?
+ * testMinStack['_minStack'] //?
+ * testMinStack['_maxStack'] //?
+ * --------------
+ * testMinStack.pop() //?
+ * testMinStack.min() //?
+ * testMinStack.max() //?
+ * --------------
+ * testMinStack.pop() //?
+ * testMinStack.min() //?
+ * testMinStack.max() //?
+ * --------------
+ * testMinStack['_storage'] //?
+ * testMinStack['_minStack'] //?
+ * testMinStack['_maxStack'] //?
+ * --------------
+ * testMinStack.push(8)
+ * testMinStack.min() //?
+ * testMinStack.max() //?
+ * --------------
+ * testMinStack['_storage'] //?
+ * testMinStack['_minStack'] //?
+ * testMinStack['_maxStack'] //?
+ * --------------
+ * testMinStack.pop() //?
+ * testMinStack.pop() //?
+ * testMinStack.pop() //?
+ * --------------
+ * testMinStack['_storage'] //?
+ * testMinStack['_minStack'] //?
+ * testMinStack['_maxStack'] //?
+ */
--- packages/practical-intro-to-data-structures/src/stack.ts
+++ packages/practical-intro-to-data-structures/src/stack.ts
@@ -1,13 +1,12 @@
-/* eslint-disable no-inline-comments */
 /* eslint-disable no-undefined, no-underscore-dangle, @typescript-eslint/ban-ts-comment, no-plusplus */
 
-export interface StackInterface<T> {
+export interface Stack<T> {
   /**
-   * Adds a member to the collection
-   * @throws `stack overflow` if the stack has grown over the bounded size
-   * @param {T} x - value to add to the Stack
+   * Returns the most Head of the stack without removing it
+   * @throws `stack underflow` if the stack is empty
+   * @returns {T}
    */
-  push(x: T): void
+  peek(): undefined | T
 
   /**
    * Removes and returns the most recently added member to the collection
@@ -17,38 +16,33 @@ export interface StackInterface<T> {
   pop(): undefined | T
 
   /**
-   * Returns the most Head of the stack without removing it
-   * @throws `stack underflow` if the stack is empty
-   * @returns {T}
+   * Adds a member to the collection
+   * @throws `stack overflow` if the stack has grown over the bounded size
+   * @param {T} x - value to add to the Stack
    */
-  peek(): undefined | T
+  push(x: T): void
 }
 
 /** Class representing a Stack. */
-export class Stack<T> implements StackInterface<T> {
-  private readonly _stack: Record<number, T>
-
+export class StackClass<T> implements Stack<T> {
   private _length: number
+
+  private readonly _stack: Record<number, T>
 
   public constructor() {
     this._stack = {}
     this._length = 0
   }
 
-  private incrementLength(): void {
-    this._length++
-  }
-
-  private decrementLength(): void {
-    this._length--
-  }
-
   /**
-   * Adds a member to the collection
+   * Returns the most Head of the stack without removing it
    */
-  public push(value: T): void {
-    this._stack[this._length] = value
-    this.incrementLength()
+  public peek(): undefined | T {
+    if (this._length > 0) {
+      const lastIdx = this._length - 1
+      return this._stack[lastIdx]
+    }
+    return undefined
   }
 
   /**
@@ -66,36 +60,39 @@ export class Stack<T> implements StackInterface<T> {
   }
 
   /**
-   * Returns the most Head of the stack without removing it
+   * Adds a member to the collection
    */
-  public peek(): undefined | T {
-    if (this._length > 0) {
-      const lastIdx = this._length - 1
-      return this._stack[lastIdx]
-    }
-    return undefined
+  public push(value: T): void {
+    this._stack[this._length] = value
+    this.incrementLength()
   }
 
   public toString(): string {
-    return JSON.stringify({ length: this._length, stack: this._stack }, null, 2)
+    return JSON.stringify(this, null, 2)
+  }
+
+  private decrementLength(): void {
+    this._length--
+  }
+
+  private incrementLength(): void {
+    this._length++
   }
 }
 
-// Examples
-
-const testStack = new Stack<string>()
-testStack.peek() // undefined
-
-testStack.push('first')
-testStack.peek() // 'first'
-testStack.toString() // '{ length: 1, stack: { 0: "first" } }'
-
-testStack.push('second')
-testStack.peek() // 'second
-testStack.toString() // '{ length: 3, stack: { 0: "first", 1: "second", 2: "third" } }'
-
-testStack.push('third')
-testStack.toString() // '{ "length": 3, "stack": { "0": "first", "1": "second", "2": "third" } }'
-testStack.peek() // 'third
-testStack.pop() // 'third
-testStack.toString() // '{ "length": 2, "stack": { "0": "first", "1": "second" } }'
+/*
+ * Examples
+ * const testStack = new StackClass<string>()
+ * testStack.peek() // undefined
+ * testStack.push('first')
+ * testStack.peek() // 'first'
+ * testStack.toString() // '{ length: 1, stack: { 0: "first" } }'
+ * testStack.push('second')
+ * testStack.peek() // 'second
+ * testStack.toString() // '{ length: 3, stack: { 0: "first", 1: "second", 2: "third" } }'
+ * testStack.push('third')
+ * testStack.toString() // '{ "length": 3, "stack": { "0": "first", "1": "second", "2": "third" } }'
+ * testStack.peek() // 'third
+ * testStack.pop() // 'third
+ * testStack.toString() // '{ "length": 2, "stack": { "0": "first", "1": "second" } }'
+ */
