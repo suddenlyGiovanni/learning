14131d52de1bd4965904139774d6b7c1f1df78bf
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-14 14:14:45 +0200

    feat(data-structures): add exercise remove duplicates chr from string

---
+++ packages/practical-intro-to-data-structures/src/12-hash-table-remove-dups.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-inline-comments */
+/*
+ * Write a function that removes duplicates characters from a string
+ */
+
+import assert from './assert'
+
+/**
+ * Removes duplicates characters from a string
+ * @param {string} str - the string
+ * @return {string} - a string without duplicates
+ */
+export const removeDuplicates = (str: string): string => {
+  const duplicates = new Set<string>()
+  return str.split('').reduce((acc, chr) => {
+    return duplicates.has(chr)
+      ? acc // There is already a chr, so drop the current one
+      : (duplicates.add(chr), acc + chr) // add the chr to the duplicate map and also to the output
+  }, '')
+}
+
+export const main = (): void => {
+  console.log(removeDuplicates('mississippi'))
+  assert(removeDuplicates('mississippi') === 'misp')
+}
+// main()
