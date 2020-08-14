03c49207483da3a8bc493e96d244494810a189f4
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-14 22:35:29 +0200

    feat(data-structures): add exercise on boolean matrix

---
+++ packages/practical-intro-to-data-structures/src/16-boolean-matrix.ts
@@ -0,0 +1,87 @@
+/* eslint-disable max-statements */
+/* eslint-disable no-underscore-dangle */
+/*
+ * Given a boolean matrix, modify it such that if a matrix cell matrix[i][j] is 1 (true) then
+ * make all the cells of ith row and jth column as 1.
+ * More here: https://www.geeksforgeeks.org/a-boolean-matrix-question/
+ */
+
+/**
+ * Mark one on both row and colum
+ * @template T
+ * @param {Array<Array<T>>} matrix - the matrix
+ * @return {Array<Array<T>>} - the transformed matrix
+ */
+export const markOnes = (matrix: Array<Array<1 | 0>>): Array<Array<1 | 0>> => {
+  const _matrix = matrix.slice()
+  const row = Array(_matrix.length).fill(0) as (0 | 1)[]
+  const column = Array(_matrix[0].length).fill(0) as (0 | 1)[]
+  const rowsNumber = _matrix.length
+  const columnNumber = _matrix[0].length
+
+  /* Store the rows and columns to be marked as 1 in row[] and col[] arrays respectively */
+  for (let i = 0; i < rowsNumber; i++) {
+    for (let j = 0; j < columnNumber; j++) {
+      if (_matrix[i][j] === 1) {
+        row[i] = 1
+        column[j] = 1
+      }
+    }
+  }
+  /* Modify the input matrix mat[] using the above constructed row[] and col[] arrays */
+  for (let i = 0; i < rowsNumber; i++) {
+    for (let j = 0; j < columnNumber; j++) {
+      if (row[i] === 1 || column[j] === 1) {
+        _matrix[i][j] = 1
+      }
+    }
+  }
+
+  return _matrix
+}
+
+export const main = (): void => {
+  /*
+   * Expected output:
+   * [
+   *   [1, 1],
+   *   [1, 0],
+   * ]
+   */
+  console.log(
+    markOnes([
+      [1, 0],
+      [0, 0],
+    ])
+  )
+
+  /*
+   * Expected output:
+   * [
+   *   [ 0, 0, 1 ],
+   *   [ 1, 1, 1 ]
+   * ]
+   */
+  console.log(
+    markOnes([
+      [0, 0, 0],
+      [0, 0, 1],
+    ])
+  )
+
+  /*
+   * [
+   *   [1, 1, 1, 1],
+   *   [1, 1, 1, 1],
+   *   [1, 0, 1, 1],
+   * ]
+   */
+  console.log(
+    markOnes([
+      [1, 0, 0, 1],
+      [0, 0, 1, 0],
+      [0, 0, 0, 0],
+    ])
+  )
+}
+// main()
