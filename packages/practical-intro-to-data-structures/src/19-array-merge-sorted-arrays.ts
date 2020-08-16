1f80d41396412d96f62f1957dd3d79618116fa2c
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-16 18:53:26 +0200

    feat(data-strucutres): add exercise merge two sorted arrays in linear time

---
+++ packages/practical-intro-to-data-structures/src/19-array-merge-sorted-arrays.ts
@@ -0,0 +1,67 @@
+/* eslint-disable max-statements */
+/**
+ * # Merge two sorted arrays
+ * Given two sorted arrays, the task is to merge them in a sorted manner.
+ *
+ * Input: arr1[] = {1, 3, 4, 5}, arr2[] = {2, 4, 6, 8}
+ * Output: arr3[] = {1, 2, 3, 4, 4, 5, 6, 8}
+ *
+ * Input: arr1[] = { 5, 8, 9}, arr2[] = {4, 7, 8}
+ * Output: arr3[] = {4, 5, 7, 8, 8, 9}
+ */
+
+import assert from 'assert'
+
+/**
+ * Returns a sorted, merged list
+ * @template T
+ * @param {T[]} left - first sorted array
+ * @param {T[]} right - second sorted array
+ * @return {T[]} - the merged array
+ */
+export const merge = <T>(left: T[], right: T[]): T[] => {
+  const accumulator: T[] = []
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    const leftValue = left[leftIndex]
+    const rightValue = right[rightIndex]
+    if (leftValue < rightValue) {
+      accumulator.push(leftValue)
+      leftIndex += 1
+    } else {
+      accumulator.push(rightValue)
+      rightIndex += 1
+    }
+  }
+
+  while (leftIndex < left.length) {
+    const leftValue = left[leftIndex]
+    accumulator.push(leftValue)
+    leftIndex += 1
+  }
+
+  while (rightIndex < right.length) {
+    const rightValue = right[rightIndex]
+    accumulator.push(rightValue)
+    rightIndex += 1
+  }
+
+  return accumulator
+}
+
+
+// Example
+export const main = (): void => {
+  const listA1 = [1, 3, 4, 5]
+  const listA2 = [2, 4, 6, 8]
+  const expectedListOutputA = [1, 2, 3, 4, 4, 5, 6, 8]
+  assert.deepStrictEqual(merge(listA1, listA2), expectedListOutputA)
+
+  const listB1 = [5, 8, 9]
+  const listB2 = [4, 7, 8]
+  const expectedListOutputB = [4, 5, 7, 8, 8, 9]
+  assert.deepStrictEqual(merge(listB1, listB2), expectedListOutputB)
+}
+// main()
