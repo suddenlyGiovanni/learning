3b07ff8021155429421b6f2567fb7a9cdf261778
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-16 17:35:04 +0200

    feat(data-structures): add exercise on string urlify

---
+++ packages/practical-intro-to-data-structures/src/18-string-urlify.ts
@@ -0,0 +1,30 @@
+/**
+ * # URLify a given string (Replace spaces is %20)
+ * Write a method to replace all the spaces in a string with ‘%20’.
+ * You may assume that the string has sufficient space at the end to hold the additional
+ * characters, and that you are given the “true” length of the string.
+ *
+ * @example
+ * Input : "Mr John Smith", 13
+ * Output : Mr%20John%20Smith
+ *
+ * Input : "Mr John Smith   ", 13
+ * Output : Mr%20John%20Smith
+ */
+
+import assert from './assert'
+
+export const replaceSpaces = (str: string): string =>
+  str
+    .trim()
+    .split('')
+    .reduce((acc, chr) => {
+      return chr === ' ' ? `${acc}%20` : acc + chr
+    }, '')
+
+// Examples:
+export const main = (): void => {
+  assert(replaceSpaces('Mr John Smith') === 'Mr%20John%20Smith')
+  assert(replaceSpaces('Mr John Smith   ') === 'Mr%20John%20Smith')
+}
+// main()
