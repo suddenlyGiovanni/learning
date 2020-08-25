87da547a8b4cca581c043a536988e10f4530de86
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-25 13:06:54 +0200

    feat(tree-and-graph-data-structures): add Queue<T> data structure

--- packages/tree-and-graph-data-structures/src/stack/stack.interface.ts
+++ packages/tree-and-graph-data-structures/src/stack/stack.interface.ts
@@ -5,35 +5,36 @@
  */
 export interface IStack<T> {
   /**
-   * Reset the Stack to
+   * Reset the Stack
    * @memberof IStack
    */
   clear(): void
 
   /**
    * Returns if the stack is empty or not
-   * @return {boolean} - whether or not the stack is empty
+   * @returns {boolean} - whether or not the stack is empty
+   * @memberof IStack
    */
   isEmpty(): boolean
 
   /**
    * Returns the most Head of the stack without removing it
-   * @throws `stack underflow` if the stack is empty
-   * @returns {T}
+   * @returns {(undefined | T)}
+   * @memberof IStack
    */
   peek(): undefined | T
 
   /**
    * Removes and returns the most recently added member to the collection
-   * @throws `stack underflow` if the stack is empty
-   * @returns {T}
+   * @returns {(undefined | T)}
+   * @memberof IStack
    */
   pop(): undefined | T
 
   /**
    * Adds a member to the collection
-   * @throws `stack overflow` if the stack has grown over the bounded size
    * @param {T} x - value to add to the Stack
+   * @memberof IStack
    */
   push(x: T): void
 
